cbeb618e6c1cb4ba385e6d6794d02b1532eca738
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-08-14 23:00:54 +0200

    test: update better shell tests (#141)

---
+++ test/cases/better-shell-syntax-#034.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+command_args=("/usr/bin/test-program" "arg1" "arg2")
+posts=(1 3 4 10)
+
+echo "Start"
+
+for post in "${posts[@]}"; do
+	"${command_args[@]}" "$post"
+    "${command_args[@]}" "$post"
+done
+
+echo "End"
+"${command_args[@]}" "$post"
+ "${command_args[@]}" "$post"
+	"${command_args[@]}" "$post"
+${command_args[@]} "$post"
+
